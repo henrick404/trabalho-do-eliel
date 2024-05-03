7e698e680643d09bb25d5eeb0edfda0688e4307c
henrick404 <160244828+henrick404@users.noreply.github.com>
2024-05-03 09:15:37 -0300

    Add files via upload

---
+++ umscrit.js
@@ -0,0 +1,12 @@
+let nome = 'lua'
+let idade = 25
+let numerodevendas = 50
+let saldoDisponivel = 1000
+let mensagemDeErro = 'Erro! Preencha todos os campos.'
+window.prompt("seu nome", nome)
+window.prompt("sua idade", idade)
+if (idade >= 18) {
+    console.log("Pode tirar a habilitação")
+}
+
+
